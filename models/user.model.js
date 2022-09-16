b76d81e7bbf4ad1e2f2d637c9ca9cac3b94ed5cc
DavTheDev20 <reiddavin@outlook.com>
2022-09-15 21:09:27 -0400

    Add /login route

--- models/user.model.js
+++ models/user.model.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
   {
-    userId: { type: String, required: true },
-    email: { type: String, required: true },
+    userId: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
     passcode: { type: String, required: true },
   },
   {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,44 +7,79 @@ const userRouter = express.Router();
 const saltRounds = 10;
 const JWT_SECRET = 'secret';
 
-userRouter.post('/register', (req, res, next) => {
-  const { userId, email, passcode } = req.body;
+userRouter
+  .post('/register', (req, res, next) => {
+    const { userId, email, passcode } = req.body;
 
-  if (userId && email && passcode) {
-    bcrypt.genSalt(saltRounds, (saltingError, salt) => {
-      if (saltingError) {
-        return res.status(500).json({ success: false, error: saltingError });
-      }
-      bcrypt.hash(passcode, salt, (hashingError, hash) => {
-        if (hashingError) {
-          return res.status(500).json({ success: false, error: hashingError });
+    if (userId && email && passcode) {
+      bcrypt.genSalt(saltRounds, (saltingError, salt) => {
+        if (saltingError) {
+          return res.status(500).json({ success: false, error: saltingError });
         }
-        const newUser = new User({
-          userId: userId,
-          email: email,
-          passcode: hash,
+        bcrypt.hash(passcode, salt, (hashingError, hash) => {
+          if (hashingError) {
+            return res
+              .status(500)
+              .json({ success: false, error: hashingError });
+          }
+          const newUser = new User({
+            userId: userId,
+            email: email,
+            passcode: hash,
+          });
+          newUser
+            .save()
+            .then((result) => {
+              const jwtToken = jwt.sign(
+                { _id: result._id, userId: result.userId, email: result.email },
+                JWT_SECRET,
+                { algorithm: 'HS256' }
+              );
+              return res.status(200).json({ success: true, token: jwtToken });
+            })
+            .catch((error) => {
+              return res
+                .status(400)
+                .json({ success: false, error: error.message });
+            });
         });
-        newUser
-          .save()
-          .then((result) => {
+      });
+    } else {
+      return res.status(400).json({
+        success: false,
+        error: 'Please include userId, email, and passcode in request body.',
+      });
+    }
+  })
+  .post('/login', async (req, res, next) => {
+    const { userId, passcode } = req.body;
+
+    if (userId && passcode) {
+      const user = await User.findOne({ userId: userId });
+      if (user) {
+        bcrypt.compare(passcode, user.passcode, (bcryptError, same) => {
+          if (same) {
             const jwtToken = jwt.sign(
-              { _id: result._id, userId: result.userId, email: result.email },
+              { _id: user._id, userId: user.userId, email: user.email },
               JWT_SECRET,
               { algorithm: 'HS256' }
             );
             return res.status(200).json({ success: true, token: jwtToken });
-          })
-          .catch((error) => {
-            return res.status(400).json({ success: false, error: error });
-          });
+          } else {
+            return res
+              .status(400)
+              .json({ success: false, error: 'Incorrect password' });
+          }
+        });
+      } else {
+        return res.status(400).json({ success: false, error: 'No user found' });
+      }
+    } else {
+      return res.status(400).json({
+        success: false,
+        error: 'Please include userId and passcode in request body.',
       });
-    });
-  } else {
-    return res.status(400).json({
-      success: false,
-      error: 'Please include userId, email, and passcode in request body.',
-    });
-  }
-});
+    }
+  });
 
 module.exports = userRouter;
